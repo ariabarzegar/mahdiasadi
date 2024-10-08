1e351460746b1174f6afff516d05b7c0e1aa627d
ariabarzegar <166044451+ariabarzegar@users.noreply.github.com>
2024-10-09 03:17:26 +0330

    Update script.js 1.2

--- script.js
+++ script.js
@@ -2,31 +2,57 @@ let $ = document;
 let selectTap = $.querySelector(".tap-score");
 let selectScore = $.querySelector(".score");
 let levelNumber = $.querySelector(".level-number");
-let i = 0;
+let cart = $.querySelectorAll(".cart");
+let makhfi = $.querySelector(".makhfi");
+let main = $.querySelector("main");
+let a = 0;
+
+cart.forEach((element)=>{
+  element.addEventListener("click",()=>{
+    makhfi.classList.add("nashan")
+  })
+
+})
+
+main.addEventListener("click",()=>{
+  makhfi.classList.remove("nashan")
+})
+
 
 let addScore = () => {
-  selectScore.innerHTML = i++;
+  selectScore.innerHTML = a++;
   selectTap.classList.add("border");
   setTimeout(() => {
     selectTap.classList.remove("border");
   }, 150);
-  setlocal(i);
-  if (i > 100) {
+  setlocal(a);
+   if(a > 30000){
+    selectTap.classList.add("img4");
+    selectTap.classList.remove("img3");
+    a += 3;
+    levelNumber.innerHTML=4
+  }else if(a > 5000){
+    selectTap.classList.add("img3");
+    selectTap.classList.remove("img1");
+    a += 4;
+    levelNumber.innerHTML=3
+  }else if (a > 100) {
     selectTap.classList.add("img1");
     selectTap.classList.remove("img2");
-    i += 2;
+    a += 2;
     levelNumber.innerHTML=2
   }
+
 };
-let setlocal = (i) => {
-  localStorage.setItem("score", i);
+let setlocal = (a) => {
+  localStorage.setItem("scoreAsa", a);
 };
 
 let addLocal = () => {
-  let valueI = localStorage.getItem("score");
-  i = valueI;
-  if (i > 0) {
-    i--;
+  let valueI = localStorage.getItem("scoreAsa");
+  a = valueI;
+  if (a > 0) {
+    a--;
   }
   addScore();
 };
