d252fb24e2fac89ae38da3eb740f9e5c70bb9c12
ariabarzegar <166044451+ariabarzegar@users.noreply.github.com>
2024-10-10 00:02:15 +0330

    Update script.js 1.5

--- script.js
+++ script.js
@@ -2,36 +2,55 @@ let $ = document;
 let selectTap = $.querySelector(".tap-score");
 let selectScore = $.querySelector(".score");
 let levelNumber = $.querySelector(".level-number");
-//let cart = $.querySelectorAll(".cart");
-//let makhfi = $.querySelector(".makhfi");
-//let main = $.querySelector("main");
-let a = 0;
-
-/*cart.forEach((element)=>{
+let bod = $.querySelector("body");
+let cart = $.querySelectorAll(".cart");
+let makhfi = $.querySelector(".makhfi");
+let cartValue = $.querySelectorAll(".cc");
+// let a = 0;
+let aria=1
+cart.forEach((element)=>{
   element.addEventListener("click",()=>{
+    // makhfi.classList.toggle("nashan")
+    if ("Cart"==element.innerHTML) {
     makhfi.classList.add("nashan")
-  })
 
+    }else if ("Home"==element.innerHTML) {
+      makhfi.classList.remove("nashan")
+      }
+    console.log(element.innerHTML);
+  })
 })
 
-main.addEventListener("click",()=>{
-  makhfi.classList.remove("nashan")
-})*/
-
+cartValue.forEach((event)=>{
+  event.addEventListener("click",(event)=>{
+    if (a>3000) {
+      if (event.target.dataset.anerje=="anerje") {
+        console.log(event.target.dataset.anerje);
+        selectTap.classList.add("img4");
+        bod.classList.add("body-color")
+        makhfi.classList.remove("nashan")
+        a-=3000
+        aria=20
+        addScore()
+        setTimeout(() => {
+        selectTap.classList.remove("img4");
+        bod.classList.remove("body-color")
+          aria=1
+        }, 9000);
+      }
+      
+    }
+  })
+})
 
 let addScore = () => {
-  selectScore.innerHTML = a++;
+  selectScore.innerHTML = a+=aria;
   selectTap.classList.add("border");
   setTimeout(() => {
     selectTap.classList.remove("border");
   }, 150);
   setlocal(a);
-   if(a > 30000){
-    selectTap.classList.add("img4");
-    selectTap.classList.remove("img3");
-    a += 3;
-    levelNumber.innerHTML=4
-  }else if(a > 5000){
+if(a > 5000){
     selectTap.classList.add("img3");
     selectTap.classList.remove("img1");
     a += 4;
@@ -52,7 +71,7 @@ let addLocal = () => {
   let valueI = localStorage.getItem("scoreAsa");
   a = valueI;
   if (a > 0) {
-    a--;
+    a--
   }
   addScore();
 };
